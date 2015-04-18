e127ba04285406585ab1b225dee62f18b0497b29
Andrei Nicholson <contact@andreinicholson.com>
2015-04-18 17:04:12 -0400

    Initial commit

---
+++ TodaysDate/DateResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TodaysDate
+{
+    /// <summary>
+    /// Date resolver.
+    /// </summary>
+    public static class DateResolver
+    {
+        /// <summary>
+        /// Get today's date. Takes care of time zones and everything.
+        /// </summary>
+        /// <returns>Today's date.</returns>
+        public static DateTime TodaysDate()
+        {
+            return DateTime.Now;
+        }
+    }
+}
