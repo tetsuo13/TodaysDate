1c12de88df01953734ac626f91b2b18414e66a4e
Andrei Nicholson <contact@andreinicholson.com>
2015-04-20 23:24:03 -0400

    Basic unit tests

---
+++ TodaysDate.Test/DateResolverTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TodaysDate.Test
+{
+    [TestFixture]
+    public class DateResolverTest
+    {
+        [Test]
+        public void TodaysDateIsToday()
+        {
+            Assert.AreEqual(DateTime.Now.Date, DateResolver.TodaysDate().Date);
+        }
+    }
+}
